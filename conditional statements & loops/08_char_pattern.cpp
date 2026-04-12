2869d27c2dd1c03873cb1f7650a0c95722aacfef
1Codebox <itsalik38@gmail.com>
2026-04-12 18:01:14 +0500

    Create 08_char_pattern.cpp

---
+++ conditional statements & loops/08_char_pattern.cpp
@@ -0,0 +1,25 @@
+//print char pattern by just defining the number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "enter number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+            
+        char ch = 'A';
+        for (int j = 1; j <= n; j++)
+        {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
