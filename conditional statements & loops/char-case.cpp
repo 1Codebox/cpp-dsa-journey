8415eb0905cde0701e30b438ca5667b2d98a08e1
1Codebox <itsalik38@gmail.com>
2026-04-12 16:00:57 +0500

    Update char-case.cpp

--- conditional statements & loops/char-case.cpp
+++ conditional statements & loops/char-case.cpp
@@ -1,1 +1,22 @@
+//check Character uppercase or lowercase
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    //int a = ch;
+    cout << "Enter character: ";
+    cin >> ch;
+    
+    if (ch >= 'A' && ch <= 'Z') {    
+        cout << "Character is uppercase";        
+    }else if (ch >= 'a' && ch <= 'z') {
+        cout << "Character is lowercase";  
+    }
+    else {
+        cout << "Not an alphabet character\n";
+    }
+
+    return 0;
+}
