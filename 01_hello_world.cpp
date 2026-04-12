246b248c2df277ba4112627434777cb64febf89e
1Codebox <itsalik38@gmail.com>
2026-04-12 09:57:12 +0500

    Create 01_hello_world.cpp
    Prints "Hello World" to the console

---
+++ 01_hello_world.cpp
@@ -0,0 +1,10 @@
+// Program: Hello World in C++
+// Description: Prints "Hello World" to the console
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;
+  return 0;
+}
