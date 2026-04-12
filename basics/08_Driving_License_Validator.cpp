5addbe6e82d9fbf8e271cdab7a65d5a1c7ab61e8
1Codebox <itsalik38@gmail.com>
2026-04-12 13:48:54 +0500

    Create 08_Driving_License_Validator.cpp

---
+++ basics/08_Driving_License_Validator.cpp
@@ -0,0 +1,19 @@
+//Check if you are eligible for driving license or not. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Enter Age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "Eligible for Driving License";
+    } else {
+        cout << "Not Eligible for Driving License";
+    }
+    
+    return 0;
+}
