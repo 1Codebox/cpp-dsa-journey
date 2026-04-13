22e22ba50a2a2fa539a25c5d1906bc6c79fbca42
1Codebox <itsalik38@gmail.com>
2026-04-13 10:07:31 +0500

    Create 16_inverted_trianlge.cpp

---
+++ conditional statements & loops/16_inverted_trianlge.cpp
@@ -0,0 +1,27 @@
+//This program prints inverted triangle pattern decreasing number each row
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n; 
+    cout << "enter number: ";
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        for (int j = n; j > i; j--)
+        {
+            cout << i+1;
+        }
+        
+        cout << endl;
+    }
+    
+    return 0;
+}
