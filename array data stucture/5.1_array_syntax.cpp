94ab8cc89f387f1525c29bdb8ef9ddaa000755ad
1Codebox <itsalik38@gmail.com>
2026-05-09 14:19:09 +0500

    Create 5.1_array_syntax.cpp

---
+++ array data stucture/5.1_array_syntax.cpp
@@ -0,0 +1,15 @@
+//array data structure basic program syntax
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mark1[5] = {50, 12, 52, 32, 40};
+    mark1[0] = 100;
+    cout << mark1[0] << endl;
+    cout << mark1[1] << endl;
+    cout << mark1[2] << endl;
+    cout << mark1[3] << endl;
+    cout << mark1[4] << endl;
+    return 0;
+}
